f56aa9d912b34fa2a4cb3777a2d0ddf51527af02
JayanAXHF <sunil.chdry@gmail.com>
2025-03-10 14:26:19 +0530

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -22,21 +22,24 @@ fn main() {
         println!("No file path provided, using default paths.json");
         file_path = String::from("paths.json");
     }
+
     let mut subject = String::new();
     print!("Subject: ");
     io::stdout().flush().expect("Unable to flush stdout");
     let _ = io::stdin().read_line(&mut subject);
-    let mut subject = subject.trim().to_owned();
+    let mut subject = subject.trim();
     if subject.is_empty() {
         println!("No subject provided, using ./");
-        subject = String::from(".");
+        subject = ".";
     }
+
     let raw_json_result = std::fs::read_to_string(file_path.trim());
     let mut raw_json = String::new();
+
     if let Ok(file) = raw_json_result {
         raw_json.push_str(&file);
     } else {
-        eprintln!("Unable to read {file_path} file. Check if the file exists, and check if the file_name is correct");
+        eprintln!("Unable to read {file_path} file. Check if the file exists, and check if the filename is correct");
         return;
     }
     let parsed_json: Input = serde_json::from_str(&raw_json).expect("Unable to parse JSON");
